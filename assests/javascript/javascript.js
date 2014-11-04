cd6c42156e82d0dd7af6fc3dced4f2f76e58dc9a
Robert Gabriel <robert_gabriel@outlook.com>
2014-11-04 10:54:15 +0000

    Update javascript.js

--- assests/javascript/javascript.js
+++ assests/javascript/javascript.js
@@ -28,4 +28,48 @@ switchBack();
 
 
 
+/*
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+ 
+  
+    // imgReplace();
+    //moveOn();
+         
+});
+ 
+ 
+function imgReplace(){ // Replaces images with newer ones.
+  var oldSrc = 'http://example.com/smith.gif';
+var newSrc = 'http://assets2.ignimgs.com/2014/08/01/rszguardians-of-the-galaxy-photos-concept-art-fulljpg-8ddbc1_160w.jpg';
+$('img').attr('src', newSrc);
+     
+}
+ 
+ 
+function moveOn(){
+     
+        var w = window.innerWidth;  //gets users inner width of window
+        var h = window.innerHeight; //gets users inner height of window
+     
+    // window.location.href = "assests/index.php?w=" + w +"&h=" + h; // Passes them onto the index file.
+     
+        
+}
+
+
+
+
+
+*/
+
+
+
 
