b1ddd7438bfebd9bed44933bf045af98be35fee6
Robert Gabriel <robert_gabriel@outlook.com>
2015-04-24 22:53:09 +0100

    States Comments
    The comments for highcharts

--- assests/model/states.php
+++ assests/model/states.php
@@ -1,37 +1,26 @@
 <?php
-
-
+    
 
 // -- Class Name : states
 // -- Purpose : 
-// -- Created On :
-    
+// -- Created On : Genortate the highcharts information from the database. in a json repsonce
     class states{
-
-        
         var $database;
-
+        public
 
 // -- Function Name : __construct
 // -- Params : 
 // -- Purpose : 
-
-        public
         function __construct()   {
-            
             $this->database =  new database();
-        
         }
 
-        
-        
-        
+        public
+
 // -- Function Name : search_chart
 // -- Params : 
-// -- Purpose : 
-        public
+// -- Purpose : Gets the amount of search from the database and displays them in highcharts
         function search_chart(){
-            
             $i = 0;
             $result =   $this->database->amount_of_searches();
             while($row = mysqli_fetch_array($result)) {
