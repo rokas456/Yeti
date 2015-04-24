3dd1698baf27d8be0652eb1bdc044c68258f6d93
Robert Gabriel <robert_gabriel@outlook.com>
2015-04-24 22:46:07 +0100

    User controller comments

--- assests/model/user.php
+++ assests/model/user.php
@@ -2,40 +2,34 @@
 
 
     include_once("database.php");
-
+    
 // -- Class Name : user
 // -- Purpose : 
-// -- Created On : 
-
+// -- Created On : The user account that has features like log in , create user , sessions cotnrols etc.
     class user{
-      
         var $database;
-      
+        public
 
 // -- Function Name : __construct
 // -- Params : 
-// -- Purpose : 
-        public
+// -- Purpose :  Start the database
         function __construct()   {
             $this->database =  new database();
         }
 
-       
 
 // -- Function Name : logout
 // -- Params : 
-// -- Purpose : 
+// -- Purpose : Destory the user  and log the user out. 
         public
         function logout(){
             session_unset();
             session_destroy();
         }
 
-        
-        
 // -- Function Name : register_account
 // -- Params : 
-// -- Purpose :         
+// -- Purpose :  Register the user and valdates the information, tos ee if there there or not.
         public
         function register_account(){
             $username = $_POST['username'];
@@ -60,11 +54,10 @@
 
         }
 
-      
 
 // -- Function Name : create_sessions
 // -- Params : $results
-// -- Purpose : 
+// -- Purpose : Crates the session varables if the login is correct
         public
         function create_sessions($results){
             // session_start();
@@ -83,21 +76,21 @@
 
         }
 
-        
+
 
 // -- Function Name : get_amount_of_users
 // -- Params : 
-// -- Purpose : 
+// -- Purpose : Gets the amount of users in the database ( for the home page)
         public
         function get_amount_of_users(){
             return  $this->database->count_amount_of_users();
         }
 
-    
+
 
 // -- Function Name : sign_in
 // -- Params : 
-// -- Purpose : 
+// -- Purpose : Sign in function to see if there their or not.
         public
         function sign_in(){
             $email = $_POST['email'];
@@ -115,13 +108,22 @@
 
         }
 
-        public
+
+
+// -- Function Name : delete_account
+// -- Params : 
+// -- Purpose : Delete the account if the user requests it.
+        public 
         function delete_account(){
             $email = $_SESSION["email"];
             $username = $_SESSION["NAME"];
             $this->database->delete_account($username,$email);
         }
 
+
+// -- Function Name : update_account
+// -- Params : 
+// -- Purpose :  Update the user account wwith password etc etc.
         public
         function update_account(){
             $password = $_POST["password"];
@@ -129,21 +131,24 @@
             $this->database->update_account($password,$twitter);
             $wasItChanged =  $this->database->check_if_password_changed($password);
             $this->updateTwitter_Session($_POST["shareT"]);
+            
             if ($wasItChanged != '1' ){
                 echo 'error';
             } else {
-
                 echo 'passwordchanged';
             }
+
         }
 
-        public 
+        public
+
+// -- Function Name : updateTwitter_Session
+// -- Params : $twitter
+// -- Purpose :  If they change there twitter settings it updates the session varables, i e allow them to post results to twitter.
         function updateTwitter_Session($twitter){
-        
             $_SESSION["twitter"] =  $twitter;
-
         }
-
 
     }
+
     ?>
